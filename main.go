ba957d034c82e7a072202d1ec28e7e7aaabf7544
MatiF100 <mateuszfesz@wp.pl>
2024-05-23 23:35:53 +0200

    Ignore more errors

--- main.go
+++ main.go
@@ -42,11 +42,11 @@ func setup_db(app *App) {
 	dbString := env.Get("dbString", fmt.Sprintf("host=localhost port=5432 user=postgres dbname=postgres password=postgres sslmode=disable"))
 	client, err := ent.Open("postgres", dbString)
 	if err != nil {
-		log.Println("failed opening connection to postgres: %v", err)
+		log.Printf("failed opening connection to postgres: %v", err)
 	}
 	// Run the auto migration tool.
 	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Println("failed creating schema resources: %v", err)
+		log.Printf("failed creating schema resources: %v", err)
 	}
 
 	app.client = client
